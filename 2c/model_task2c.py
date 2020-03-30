a264df533993ea7074c26b7290e29edac3535516
Yesat <lionel.ieri@gmail.com>
2020-03-30 18:03:35 +0200

    Keras with TF and 2C start

---
+++ 2c/model_task2c.py
@@ -0,0 +1,85 @@
+"""
+CNN with 3 conv layers and a fully connected classification layer
+PATTERN RECOGNITION EXERCISE:
+Fix the three lines below marked with PR_FILL_HERE
+"""
+
+import torch.nn as nn
+
+
+class Flatten(nn.Module):
+    """
+    Flatten a convolution block into a simple vector.
+
+    Replaces the flattening line (view) often found into forward() methods of networks. This makes it
+    easier to navigate the network with introspection
+    """
+    def forward(self, x):
+        x = x.view(x.size()[0], -1)
+        return x
+
+
+class PR_CNN(nn.Module):
+    """
+    Simple feed forward convolutional neural network
+
+    Attributes
+    ----------
+    expected_input_size : tuple(int,int)
+        Expected input size (width, height)
+    conv1 : torch.nn.Sequential
+    conv2 : torch.nn.Sequential
+    conv3 : torch.nn.Sequential
+        Convolutional layers of the network
+    fc : torch.nn.Linear
+        Final classification fully connected layer
+
+    """
+
+    def __init__(self, **kwargs):
+        """
+        Creates an CNN_basic model from the scratch.
+
+        Parameters
+        ----------
+        output_channels : int
+            Number of neurons in the last layer
+        input_channels : int
+            Dimensionality of the input, typically 3 for RGB
+        """
+        super(PR_CNN, self).__init__()
+
+        # PR_FILL_HERE: Here you have to put the expected input size in terms of width and height of your input image
+        self.expected_input_size = (______, ______)
+
+        # First layer
+        self.conv1 = nn.Sequential(
+            # PR_FILL_HERE: Here you have to put the input channels, output channels ands the kernel size
+            nn.Conv2d(in_channels=______, out_channels=______, kernel_size=______, stride=3),
+            nn.LeakyReLU()
+        )
+
+        # Classification layer
+        self.fc = nn.Sequential(
+            Flatten(),
+            # PR_FILL_HERE: Here you have to put the output size of the linear layer. DO NOT change 1536!
+            nn.Linear(1536, ______)
+        )
+
+    def forward(self, x):
+        """
+        Computes forward pass on the network
+
+        Parameters
+        ----------
+        x : Variable
+            Sample to run forward pass on. (input to the model)
+
+        Returns
+        -------
+        Variable
+            Activations of the fully connected layer
+        """
+        x = self.conv1(x)
+        x = self.fc(x)
+        return x
